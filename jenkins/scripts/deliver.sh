6626ba0e8dd27e9ef3b8d4fc4c50242ace62583d
Matheus Wermuth <39707856+mawermuth@users.noreply.github.com>
2024-06-05 17:49:50 -0300

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,7 +22,7 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-echo 'ls -la'
-ls -la
+echo 'ls ./target'
+ls ./target
 set -x
-java -jar target/${NAME}-${VERSION}
+java -jar ./target/${NAME}-${VERSION}.jar
